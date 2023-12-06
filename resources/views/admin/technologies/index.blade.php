2a1db1dbda82a6087c804ff754c4c352df12edc8
alessandrodaniello105 <kanymsevement@gmail.com>
2023-12-06 15:32:39 +0100

    [add]: success+error alerts in Technology index

--- resources/views/admin/technologies/index.blade.php
+++ resources/views/admin/technologies/index.blade.php
@@ -2,29 +2,82 @@
 
 @section('content')
 
+{{-- Alert di errore --}}
+@if ($errors->any())
+
+<div class="alert alert-danger">
+
+    <ul>
+        @foreach ($errors->all() as $error)
+
+        <li>
+            {{$error}}
+        </li>
+
+        @endforeach
+    </ul>
+
+</div>
+
+
+@endif
+
+{{-- Alert di successo --}}
+@if (session('success'))
+
+    <div class="alert alert-success">
+
+        {{ session('success') }}
+
+    </div>
+
+@endif
+
+
+
     <h1>Lista Tecnologie</h1>
 
     <div class="container my-5">
 
+        {{-- INPUT FORM CREAZIONE TECNOLOGIA --}}
         <form action="{{ route('admin.technologies.store') }}" method="POST">
             @csrf
 
             <div class="input-group mb-3">
 
-                <input type="text" class="form-control" placeholder="Nome nuova tecnologia" id="new-technology-input" name="name">
+                <input type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Nome nuova tecnologia" id="new-technology-input" name="name">
 
                 <button type="submit" class="btn btn-warning">Invia</button>
 
             </div>
 
+            @error('name')
+                <ul>
+                    <li class="text-danger-ctm">{{$error}}</li>
+                </ul>
+            @enderror
+
 
         </form>
+                                {{-- EDIT BUTTON --}}
+                                {{-- <a class="btn btn-warning" href="{{route('admin.projects.edit', $project)}}">
+                                    <i class="fa-solid fa-pencil"></i>
+                                </a> --}}
 
-        <div class="card">
+        {{-- LISTA TECNOLOGIE --}}
+        <div class="card techs-wrapper">
             <ul style="list-style: none;" class="d-flex justify-content-around flex-wrap text-center">
                 @foreach ($technologies as $technology)
                 <li class="rounded my-3 btn-colored">
-                    <a style="width: 300px" class="p-4 d-inline-block" href="{{$technology->link}}">{{$technology->name}}</a>
+
+                    <a style="width: 300px" class="p-4 d-inline-block" href="{{$technology->link}}">
+                        {{$technology->name}}
+                    </a>
+
+                    <a href="{{route('admin.technologies.edit', $technology)}}" class="btn btn-warning edit-btn-ctm">
+                        <i class="fa-solid fa-pencil"></i>
+                    </a>
+
                 </li>
                 @endforeach
 
