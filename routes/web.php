e088ddd083d1062a87d1b341682b2a9b190abbea
alessandrodaniello105 <kanymsevement@gmail.com>
2023-12-06 14:28:17 +0100

    [fix]: typeProjects route+sidebar link

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ Route::middleware(['auth', 'verified'])
         Route::resource('projects', ProjectController::class);
         Route::resource('technologies', TechnologyController::class);
         Route::resource('types', TypeController::class);
-        Route::get('projects/typeProjects', [TypeController::class, 'typeProjects'])->name('projects.typeProjects');
+        Route::get('typeProjects', [TypeController::class, 'typeProjects'])->name('typeProjects');
     });
 
 require __DIR__.'/auth.php';
